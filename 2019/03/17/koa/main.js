64cdb5c66ed735a89679404a0cffcc92ea4b288d
linchen <linchen@yixin.im>
2019-03-17 14:14:05 +0800

    ajax为何不会重定向
    ajax为何不会重定向
    
    Co-Authored-By: linlin <linchen@users.noreply.github.com>

---
+++ 2019/03/17/koa/main.js
@@ -0,0 +1,18 @@
+const Koa = require('koa')
+const serve = require('koa-static')
+var Router = require('koa-router')
+const app = new Koa()
+var router = new Router()
+
+router.get('/302', (ctx, next) => {
+    ctx.status = 302
+    ctx.redirect('/302.html')
+    next()
+})
+
+app.use(router.routes())
+   .use(router.allowedMethods())
+   .use(serve('public'))
+
+app.listen(3000)
+console.log('Server on port 3000 ...');
